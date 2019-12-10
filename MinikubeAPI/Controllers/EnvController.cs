aff7cc0d273ab7f7354aa9d773561da69d7a1b12
Cédric Vigouroux <cedric.vigouroux@xpo.com>
2019-12-10 14:43:15 +0100

    2nd commit

---
+++ MinikubeAPI/Controllers/EnvController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinikubeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvController : ControllerBase
+    {
+        // GET api/env
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            List<string> output = new List<string>();
+            output.Append(string.Format("Machine Name: {0}", Environment.MachineName));
+            output.Append(string.Format("OS: {0}", Environment.OSVersion));
+            return Ok(output);
+        }
+
+        // GET api/env/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/env
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/env/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/env/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
